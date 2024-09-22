a637b1c25b3d3edc9c7aa39189ff2e289be74300
Jillian Bleggi <jillian.bleggi@gmail.com>
2024-09-22 10:46:04 -0400

    Update with Comparisons activity answers.

--- comparisons.rb
+++ comparisons.rb
@@ -24,24 +24,37 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
 # this should print: true
 
+puts "Is number_teachers less than number_students?", number_teachers < number_students
+
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
+
+puts "Is number_teachers equal to string_teachers?", number_teachers == string_teachers
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
 
+puts "Is number_teachers not equal to number_students?", number_teachers != number_students
+
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
+
+puts "Is number_students greater than or equal to 20?", number_students >= 20
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
 # this should print: false
 
+puts "Is number_students greater than or equal to 21?", number_students >= 21
+
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
+
+puts "Is number_students less than or equal to 20?", number_students <= 20
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
 
+puts "Is number_students less than or equal to 21?", number_students <=21
 
 #-------------------
 # PART 2: Articulating what you are doing
@@ -54,21 +67,25 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
+## This is evaluating whether 4 is less than 9, which will come out as true.
 
 books = 3
 puts 4 < books
 # YOU DO: Explain.
+## This is assigning "3" to the variable "books," then evaluating whether 4 is less than the variable "books". Output will be false.
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
+## This is assigning 6 to the variable "friends" and 2 to the variable "siblings", then evaluating if "friends" is greater than "siblings. Output will be true. 
+
 
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
-
+## This is assigning 9 to the variable "attendees" and 8 to the variable "meals", then evaluating whether attendees is not equal to meals. Output will be true. 
 
 #-------------------
 # PART 3: Logical Operators
@@ -90,16 +107,17 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
 # Assume that a dog is a puppy if it is less than 2 years old.
+puts loves_to_play && age <2
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: True. I used the logical operator "and" along with the comparison operator "less than" to evaluate if the dog loves to play and is under 2 years old. Both statements are true, so the answer is true!
